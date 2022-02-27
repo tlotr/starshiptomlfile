3e8a8c6fe9a7c4ee2749c8679a9a52ae58530fcb
Greg <tlotr@users.noreply.github.com>
2022-02-27 22:10:45 +0530

    Update starship.lua

--- starship.lua
+++ starship.lua
@@ -1,6 +1,6 @@
 -- A file for windows cmd requires clink to be installed
 -- Download clink from https://github.com/chrisant996/clink/releases
--- Place the file in C:\Program Files (x86)\clink\ folder
+-- Place this file in C:\Program Files (x86)\clink\ folder
 
 -- starship.lua
 load(io.popen('starship init cmd'):read("*a"))()
